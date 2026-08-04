a005ea2802d5cfe3aa2b910593ac7897db4a57ca
T0uchGr455 <matias.johndexter@gmail.com>
2026-08-04 13:55:54 +0800

    Delete sourcecode.py

--- sourcecode.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter your age: "))
-
-if age < 0:
-    print("Invalid age. Please enter a valid age.")
-elif age >= 18:
-    print("You are an adult.")
-else:
-    print("You are a minor.")
